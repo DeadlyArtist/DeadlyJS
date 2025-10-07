8118ed521d434bd999868f447d70381cda010040
Deadly Artist <ocram.siebenstein@web.de>
2025-10-07 02:02:03 +0200

    Update coreHelpers.js

--- js/coreHelpers.js
+++ js/coreHelpers.js
@@ -472,6 +472,7 @@ const _htmlStringHelpers = {
         '"': 'quot',
         '&': 'amp',
         '\'': '#39',
+        ' ': 'nbsp',
     },
     getEscapeHtmlRegex() {
         let escapeHtmlRegexString = '[';
@@ -494,7 +495,8 @@ const _htmlStringHelpers = {
         gt: '>',
         quot: '"',
         amp: '&',
-        apos: '\''
+        apos: '\'',
+        nbsp: ' ',
     },
 };
 _htmlStringHelpers.escapeHtmlRegex = _htmlStringHelpers.getEscapeHtmlRegex();
